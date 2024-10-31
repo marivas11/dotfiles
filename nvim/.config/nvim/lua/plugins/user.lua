2c94e92b13b732da68c34e279e81e12b73506d19
Manuel Rivas <marivas11@gmail.com>
2024-10-31 10:54:28 +0100

    wip

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -184,7 +184,7 @@ return {
 	},
 
 	-- use mini.starter instead of alpha
-	{ import = "lazyvim.plugins.extras.ui.mini-starter" },
+	-- { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
 	-- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
 	{ import = "lazyvim.plugins.extras.lang.json" },
@@ -230,7 +230,7 @@ return {
 
 			table.insert(opts.sources, {
 				{ name = "emoji" },
-				-- { name = "orgmode" },
+				{ name = "orgmode" },
 			})
 
 			opts.window = {
