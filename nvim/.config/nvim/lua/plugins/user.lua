5d963b96d118eb9b8765a86e18ab462ad6e3095f
Manuel Rivas <marivas11@gmail.com>
2025-01-16 13:26:05 +0100

    wip

--- nvim/.config/nvim/lua/plugins/user.lua
+++ nvim/.config/nvim/lua/plugins/user.lua
@@ -38,41 +38,41 @@ return {
 		config = true,
 	},
 
-	-- change some telescope options and a keymap to browse plugin files
-	{
-		"nvim-telescope/telescope.nvim",
-		keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-		},
-		-- change some options
-		opts = {
-			defaults = {
-				layout_strategy = "horizontal",
-				layout_config = { prompt_position = "top" },
-				sorting_strategy = "ascending",
-				winblend = 0,
-			},
-		},
-	},
-
-	-- add telescope-fzf-native
-	{
-		"telescope.nvim",
-		dependencies = {
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			config = function()
-				require("telescope").load_extension("fzf")
-			end,
-		},
-	},
-
+	-- -- change some telescope options and a keymap to browse plugin files
+	-- {
+	-- 	"nvim-telescope/telescope.nvim",
+	-- 	keys = {
+	--      -- add a keymap to browse plugin files
+	--      -- stylua: ignore
+	--      {
+	--        "<leader>fp",
+	--        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+	--        desc = "Find Plugin File",
+	--      },
+	-- 	},
+	-- 	-- change some options
+	-- 	opts = {
+	-- 		defaults = {
+	-- 			layout_strategy = "horizontal",
+	-- 			layout_config = { prompt_position = "top" },
+	-- 			sorting_strategy = "ascending",
+	-- 			winblend = 0,
+	-- 		},
+	-- 	},
+	-- },
+	--
+	-- -- add telescope-fzf-native
+	-- {
+	-- 	"telescope.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-telescope/telescope-fzf-native.nvim",
+	-- 		build = "make",
+	-- 		config = function()
+	-- 			require("telescope").load_extension("fzf")
+	-- 		end,
+	-- 	},
+	-- },
+	--
 	-- add pyright to lspconfig
 	{
 		"neovim/nvim-lspconfig",
@@ -211,63 +211,63 @@ return {
 		end,
 	},
 	-- then: setup supertab in cmp
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"hrsh7th/cmp-emoji",
-		},
-		---@param opts cmp.ConfigSchema
-		opts = function(_, opts)
-			local has_words_before = function()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
-
-			local luasnip = require("luasnip")
-			local cmp = require("cmp")
-
-			table.insert(opts.sources, {
-				{ name = "emoji" },
-				{ name = "orgmode" },
-			})
-
-			opts.window = {
-				completion = cmp.config.window.bordered({}),
-				documentation = cmp.config.window.bordered({}),
-			}
-
-			opts.mapping = vim.tbl_extend("force", opts.mapping, {
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-					-- this way you will only jump inside the snippet region
-					elseif luasnip.expand_or_jumpable() then
-						luasnip.expand_or_jump()
-					elseif has_words_before() then
-						cmp.complete()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			})
-			-- Set view to follow cursor while typing
-			opts.view = {
-				entries = {
-					follow_cursor = true,
-				},
-			}
-		end,
-	},
+	-- {
+	-- 	"hrsh7th/nvim-cmp",
+	-- 	dependencies = {
+	-- 		"hrsh7th/cmp-emoji",
+	-- 	},
+	-- 	---@param opts cmp.ConfigSchema
+	-- 	opts = function(_, opts)
+	-- 		local has_words_before = function()
+	-- 			unpack = unpack or table.unpack
+	-- 			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	-- 			return col ~= 0
+	-- 				and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+	-- 		end
+	--
+	-- 		local luasnip = require("luasnip")
+	-- 		local cmp = require("cmp")
+	--
+	-- 		table.insert(opts.sources, {
+	-- 			{ name = "emoji" },
+	-- 			{ name = "orgmode" },
+	-- 		})
+	--
+	-- 		opts.window = {
+	-- 			completion = cmp.config.window.bordered({}),
+	-- 			documentation = cmp.config.window.bordered({}),
+	-- 		}
+	--
+	-- 		opts.mapping = vim.tbl_extend("force", opts.mapping, {
+	-- 			["<Tab>"] = cmp.mapping(function(fallback)
+	-- 				if cmp.visible() then
+	-- 					cmp.select_next_item()
+	-- 				-- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+	-- 				-- this way you will only jump inside the snippet region
+	-- 				elseif luasnip.expand_or_jumpable() then
+	-- 					luasnip.expand_or_jump()
+	-- 				elseif has_words_before() then
+	-- 					cmp.complete()
+	-- 				else
+	-- 					fallback()
+	-- 				end
+	-- 			end, { "i", "s" }),
+	-- 			["<S-Tab>"] = cmp.mapping(function(fallback)
+	-- 				if cmp.visible() then
+	-- 					cmp.select_prev_item()
+	-- 				elseif luasnip.jumpable(-1) then
+	-- 					luasnip.jump(-1)
+	-- 				else
+	-- 					fallback()
+	-- 				end
+	-- 			end, { "i", "s" }),
+	-- 		})
+	-- 		-- Set view to follow cursor while typing
+	-- 		opts.view = {
+	-- 			entries = {
+	-- 				follow_cursor = true,
+	-- 			},
+	-- 		}
+	-- 	end,
+	-- },
 }
