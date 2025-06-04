366120899474e80db2c445a22fdf58bbcada43e1
Manuel Rivas <marivas11@gmail.com>
2025-06-04 09:46:07 +0200

    upgrade conf avante

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -6,10 +6,14 @@ return {
 	opts = {
 		---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
 		provider = "copilot", -- Recommend using Claude
-		copilot = {
-			model = "claude-3.5-sonnet", -- o1-preview | o1-mini | claude-3.5-sonnet
-			temperature = 0,
-			max_tokens = 8192,
+		providers = {
+			copilot = {
+				model = "claude-3.5-sonnet", -- o1-preview | o1-mini | claude-3.5-sonnet
+				extra_request_body = {
+					temperature = 0,
+					max_tokens = 8192,
+				},
+			},
 		},
 		auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
 		behaviour = {
